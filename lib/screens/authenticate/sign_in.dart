13ec309d730e8f8b886c26c0b1a906dc918d2bcf
bashar <bash996bn@gmail.com>
2020-07-17 01:26:23 +0300

    icons

--- lib/screens/authenticate/sign_in.dart
+++ lib/screens/authenticate/sign_in.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:reserve_it/hotel_app_theme.dart';
 import "package:font_awesome_flutter/font_awesome_flutter.dart";
 import 'package:reserve_it/services/auth.dart';
+import 'package:reserve_it/shared/loading.dart';
 
 class SignIn extends StatefulWidget {
   final Function toggleView;
@@ -18,99 +19,109 @@ class _SignInState extends State<SignIn> {
   String email = '';
   String password = '';
   String error = '';
+  bool loading = false;
   @override
   Widget build(BuildContext context) {
-    return Theme(
-      data: HotelAppTheme.buildLightTheme(),
-      child: Scaffold(
-        appBar: AppBar(
-          actions: [
-            FlatButton.icon(
-                onPressed: () {
-                  widget.toggleView();
-                },
-                icon: Icon(Icons.person_add),
-                label: Text("Sign up")),
-          ],
-          elevation: 0,
-          backgroundColor: Colors.white,
-          title: Text(
-            'Sign in',
-            style: TextStyle(
-              color: Colors.black54,
-            ),
-          ),
-        ),
-        body: Container(
-          color: Colors.white,
-          padding: EdgeInsets.symmetric(vertical: 20, horizontal: 50),
-          child: Form(
-            key: _formKey,
-            child: Column(
-              children: [
-                SizedBox(
-                  height: 20.0,
-                ),
-                Image.asset(
-                  'assets/images/hotel/log.jpg',
-                  height: 80,
-                ),
-                SizedBox(
-                  height: 20.0,
-                ),
-                TextFormField(
-                  decoration: inputDecoration().copyWith(hintText: 'Email'),
-                  validator: (val) => val.isEmpty ? 'Enter an Email' : null,
-                  onChanged: (val) {
-                    setState(() => email = val);
-                  },
-                ),
-                SizedBox(
-                  height: 20.0,
-                ),
-                TextFormField(
-                  decoration: inputDecoration().copyWith(hintText: 'Password'),
-                  validator: (val) => val.length < 6
-                      ? 'the password is less thn 5 characters'
-                      : null,
-                  obscureText: true,
-                  onChanged: (val) {
-                    setState(() => password = val);
-                  },
-                ),
-                SizedBox(height: 20.0),
-                RaisedButton(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10.0),
-                  ),
-                  onPressed: () async {
-                    dynamic result =
-                        _auth.signInWithEmailAndPassword(email, password);
-                    if (_formKey.currentState.validate()) {
-                      if (result == null) {
-                        setState(() => error = 'Worng credentials');
-                      }
-                    }
-                  },
-                  color: Colors.purple[600],
-                  child: Text(
-                    'Sign in',
-                    style: TextStyle(color: Colors.white),
+    return loading
+        ? Loading()
+        : Theme(
+            data: HotelAppTheme.buildLightTheme(),
+            child: Scaffold(
+              appBar: AppBar(
+                actions: [
+                  FlatButton.icon(
+                      onPressed: () {
+                        widget.toggleView();
+                      },
+                      icon: Icon(Icons.person_add),
+                      label: Text("Sign up")),
+                ],
+                elevation: 0,
+                backgroundColor: Colors.white,
+                title: Text(
+                  'Sign in',
+                  style: TextStyle(
+                    color: Colors.black54,
                   ),
                 ),
-                SizedBox(
-                  height: 20,
+              ),
+              body: Container(
+                color: Colors.white,
+                padding: EdgeInsets.symmetric(vertical: 20, horizontal: 50),
+                child: Form(
+                  key: _formKey,
+                  child: Column(
+                    children: [
+                      SizedBox(
+                        height: 20.0,
+                      ),
+                      Image.asset(
+                        'assets/images/hotel/log.jpg',
+                        height: 80,
+                      ),
+                      SizedBox(
+                        height: 20.0,
+                      ),
+                      TextFormField(
+                        decoration:
+                            inputDecoration().copyWith(hintText: 'Email'),
+                        validator: (val) =>
+                            val.isEmpty ? 'Enter an Email' : null,
+                        onChanged: (val) {
+                          setState(() => email = val);
+                        },
+                      ),
+                      SizedBox(
+                        height: 20.0,
+                      ),
+                      TextFormField(
+                        decoration:
+                            inputDecoration().copyWith(hintText: 'Password'),
+                        validator: (val) => val.length < 6
+                            ? 'the password is less thn 5 characters'
+                            : null,
+                        obscureText: true,
+                        onChanged: (val) {
+                          setState(() => password = val);
+                        },
+                      ),
+                      SizedBox(height: 20.0),
+                      RaisedButton(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10.0),
+                        ),
+                        onPressed: () async {
+                          dynamic result =
+                              _auth.signInWithEmailAndPassword(email, password);
+                          if (_formKey.currentState.validate()) {
+                            setState(() => loading = true);
+                            if (result == null) {
+                              setState(() {
+                                error = 'Worng credentials';
+                                loading = false;
+                              });
+                            }
+                          }
+                        },
+                        color: Colors.purple[600],
+                        child: Text(
+                          'Sign in',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Text(
+                        error,
+                        style: TextStyle(color: Colors.red, fontSize: 16.0),
+                      )
+                    ],
+                  ),
                 ),
-                Text(
-                  error,
-                  style: TextStyle(color: Colors.red, fontSize: 16.0),
-                )
-              ],
+              ),
             ),
-          ),
-        ),
-      ),
-    );
+          );
   }
 
   void signIn() async {
--- lib/screens/authenticate/sign_up.dart
+++ lib/screens/authenticate/sign_up.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:reserve_it/services/auth.dart';
+import 'package:reserve_it/shared/loading.dart';
 
 import '../../hotel_app_theme.dart';
 
@@ -19,100 +20,109 @@ class _SignUpState extends State<SignUp> {
   String email = '';
   String password = '';
   String error = '';
+  bool loading = false;
   @override
   Widget build(BuildContext context) {
-    return Theme(
-      data: HotelAppTheme.buildLightTheme(),
-      child: Scaffold(
-        appBar: AppBar(
-          actions: [
-            FlatButton.icon(
-                onPressed: () {
-                  widget.toggleView();
-                },
-                icon: Icon(Icons.person),
-                label: Text("Sign in")),
-          ],
-          elevation: 0,
-          backgroundColor: Colors.white,
-          title: Text(
-            'Sign Up',
-            style: TextStyle(
-              color: Colors.black54,
-            ),
-          ),
-        ),
-        body: Container(
-          color: Colors.white,
-          padding: EdgeInsets.symmetric(vertical: 20, horizontal: 50),
-          child: Form(
-            key: _formKey,
-            child: Column(
-              children: [
-                SizedBox(
-                  height: 20.0,
-                ),
-                Image.asset(
-                  'assets/images/hotel/sign.jpg',
-                  height: 70,
-                ),
-                SizedBox(
-                  height: 20.0,
-                ),
-                TextFormField(
-                  decoration: inputDecoration().copyWith(hintText: 'Email'),
-                  validator: (val) => val.isEmpty ? 'Enter an Email' : null,
-                  onChanged: (val) {
-                    setState(() => email = val);
-                  },
-                ),
-                SizedBox(
-                  height: 20.0,
-                ),
-                TextFormField(
-                  decoration: inputDecoration().copyWith(hintText: 'Passowrd'),
-                  validator: (val) => val.length < 6
-                      ? 'the password is less thn 5 characters'
-                      : null,
-                  obscureText: true,
-                  onChanged: (val) {
-                    setState(() => password = val);
-                  },
-                ),
-                SizedBox(height: 20.0),
-                RaisedButton(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10.0),
-                  ),
-                  onPressed: () async {
-                    if (_formKey.currentState.validate()) {
-                      dynamic result = await _auth.signUpWithEmailAndPassword(
-                          email, password);
-                      if (result == null) {
-                        setState(() => error =
-                            'somthing went wrong, please check your connectin and try Again');
-                      }
-                    }
-                  },
-                  color: Colors.purple[600],
-                  child: Text(
-                    'Sign up',
-                    style: TextStyle(color: Colors.white),
+    return loading
+        ? Loading()
+        : Theme(
+            data: HotelAppTheme.buildLightTheme(),
+            child: Scaffold(
+              appBar: AppBar(
+                actions: [
+                  FlatButton.icon(
+                      onPressed: () {
+                        widget.toggleView();
+                      },
+                      icon: Icon(Icons.person),
+                      label: Text("Sign in")),
+                ],
+                elevation: 0,
+                backgroundColor: Colors.white,
+                title: Text(
+                  'Sign Up',
+                  style: TextStyle(
+                    color: Colors.black54,
                   ),
                 ),
-                SizedBox(
-                  height: 20,
+              ),
+              body: Container(
+                color: Colors.white,
+                padding: EdgeInsets.symmetric(vertical: 20, horizontal: 50),
+                child: Form(
+                  key: _formKey,
+                  child: Column(
+                    children: [
+                      SizedBox(
+                        height: 20.0,
+                      ),
+                      Image.asset(
+                        'assets/images/hotel/sign.jpg',
+                        height: 70,
+                      ),
+                      SizedBox(
+                        height: 20.0,
+                      ),
+                      TextFormField(
+                        decoration:
+                            inputDecoration().copyWith(hintText: 'Email'),
+                        validator: (val) =>
+                            val.isEmpty ? 'Enter an Email' : null,
+                        onChanged: (val) {
+                          setState(() => email = val);
+                        },
+                      ),
+                      SizedBox(
+                        height: 20.0,
+                      ),
+                      TextFormField(
+                        decoration:
+                            inputDecoration().copyWith(hintText: 'Passowrd'),
+                        validator: (val) => val.length < 6
+                            ? 'the password is less thn 5 characters'
+                            : null,
+                        obscureText: true,
+                        onChanged: (val) {
+                          setState(() => password = val);
+                        },
+                      ),
+                      SizedBox(height: 20.0),
+                      RaisedButton(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10.0),
+                        ),
+                        onPressed: () async {
+                          if (_formKey.currentState.validate()) {
+                            setState(() => loading = true);
+                            dynamic result = await _auth
+                                .signUpWithEmailAndPassword(email, password);
+                            if (result == null) {
+                              setState(() {
+                                error =
+                                    'somthing went wrong, please check your connectin and try Again';
+                              });
+                            }
+                          }
+                        },
+                        color: Colors.purple[600],
+                        child: Text(
+                          'Sign up',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Text(
+                        error,
+                        style: TextStyle(color: Colors.red, fontSize: 16.0),
+                      )
+                    ],
+                  ),
                 ),
-                Text(
-                  error,
-                  style: TextStyle(color: Colors.red, fontSize: 16.0),
-                )
-              ],
+              ),
             ),
-          ),
-        ),
-      ),
-    );
+          );
   }
 
   InputDecoration inputDecoration() {
